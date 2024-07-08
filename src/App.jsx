1c46fbee22e4cb4714aed1422daf990508872188
MohammadKerdasi <mohammadkerdasi003@gmail.com>
2024-07-08 16:43:55 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBarCom from "./Components/NavBar/NavBarCom";
 import Footer from "./Components/Footer/Footer";
@@ -6,6 +6,16 @@ import "./App.css";
 import ScrollToTop from "./Components/ScrollToTop";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import Home from './Pages/Home.jsx';
+import Courses from './Pages/Courses.jsx';
+import Blog from './Pages/Blog.jsx';
+import Contact from './Pages/Contact.jsx';
+import FAQs from './Pages/FAQs.jsx';
+// import LoginRegister from './Pages/LoginRegister.jsx';
+import CourseSingle from './Pages/CourseSingle.jsx';
+import BlogSingle from './Pages/BlogSingle.jsx';
+import Errorpage from './Pages/Errorpage.jsx';
+import LoginandRegister from "./Pages/LoginRegister.jsx";
 
 function App() {
   const scrollToTop = () => {
@@ -21,7 +31,17 @@ function App() {
         <ScrollToTop />
         <button onClick={scrollToTop} className="rb-scroll-to-top-button"><FontAwesomeIcon icon={faArrowUp} /></button>
         <NavBarCom />
-        <Outlet />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/Courses' element={<Courses/>} />
+              <Route path='/Courses/:courseTitle' element={<CourseSingle  />} />
+              <Route path='/Blog' element={<Blog />} />
+              <Route path='/Contact' element={<Contact  />} />
+              <Route path='/FAQs' element={<FAQs  />} />
+              <Route path='/LoginRegister' element={<LoginandRegister  />} />
+              <Route path='/Blog/:blogTitle' element={<BlogSingle  />} />
+              <Route path='/Error' element={<Errorpage  />} />
+          </Routes>
         <Footer />
       </section>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,66 +1,71 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
-import Home from './Pages/Home.jsx';
-import Courses from './Pages/Courses.jsx';
-import Blog from './Pages/Blog.jsx';
-import Contact from './Pages/Contact.jsx';
-import FAQs from './Pages/FAQs.jsx';
-import LoginRegister from './Pages/LoginRegister.jsx';
-import CourseSingle from './Pages/CourseSingle.jsx';
-import BlogSingle from './Pages/BlogSingle.jsx';
-import Errorpage from './Pages/Errorpage.jsx';
+// import Home from './Pages/Home.jsx';
+// import Courses from './Pages/Courses.jsx';
+// import Blog from './Pages/Blog.jsx';
+// import Contact from './Pages/Contact.jsx';
+// import FAQs from './Pages/FAQs.jsx';
+// import LoginRegister from './Pages/LoginRegister.jsx';
+// import CourseSingle from './Pages/CourseSingle.jsx';
+// import BlogSingle from './Pages/BlogSingle.jsx';
+// import Errorpage from './Pages/Errorpage.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "Courses",
-        element: <Courses />,
-      },
-      {
-        path: "Courses/:courseTitle",
-        element: <CourseSingle />,
-      },
-      {
-        path: "Blog",
-        element: <Blog />,
-      },
-      {
-        path: "Contact",
-        element: <Contact />,
-      },
-      {
-        path: "FAQs",
-        element: <FAQs />,
-      },
-      {
-        path: "LoginRegister",
-        element: <LoginRegister />,
-      },
-      {
-        path: "Blog/:blogTitle",
-        element: <BlogSingle />,
-      },
-      {
-        path: "Error",
-        element: <Errorpage />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "Courses",
+//         element: <Courses />,
+//       },
+//       {
+//         path: "Courses/:courseTitle",
+//         element: <CourseSingle />,
+//       },
+//       {
+//         path: "Blog",
+//         element: <Blog />,
+//       },
+//       {
+//         path: "Contact",
+//         element: <Contact />,
+//       },
+//       {
+//         path: "FAQs",
+//         element: <FAQs />,
+//       },
+//       {
+//         path: "LoginRegister",
+//         element: <LoginRegister />,
+//       },
+//       {
+//         path: "Blog/:blogTitle",
+//         element: <BlogSingle />,
+//       },
+//       {
+//         path: "Error",
+//         element: <Errorpage />,
+//       },
+//     ],
+//   },
+// ]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter basename='/EduPressX71'>
+    <App/>
+    </BrowserRouter>
   </React.StrictMode>,
 );
